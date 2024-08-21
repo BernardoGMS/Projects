9d512dce4943b2f829a8a38e1f8494f94f02dd50
Goncalo Bernardo <goncalo.m.s.bernardo@gmail.com>
2024-08-21 18:32:50 +0100

    NN Project added

--- 2-Data Science/2.4-Elections Theory/jefferson_data.py
+++ 2-Data Science/2.4-Elections Theory/jefferson_data.py
@@ -7,10 +7,10 @@ import random as rd
 def compute_pattern_divisor(n_el, n_distr):
     return n_el / n_distr
 
-def jefferson(votes, result , n_distr, patt_div_r, n_decimal, inc, n_el):
+def jefferson(votes, result , n_distr, patt_div_r, n_decimal, inc, n_el, mean_dev):
     soma = sum(np.floor(result))
     n_decimal_it = n_decimal
-
+    
     it = 1
     if (soma > n_distr):
         while (soma != n_distr):
@@ -26,8 +26,9 @@ def jefferson(votes, result , n_distr, patt_div_r, n_decimal, inc, n_el):
                 patt_div_r = patt_div_r - 3 * inc
                 inc = inc / 100
                 n_decimal_it += 1
-                if (it>=10000000):
-                    break
+
+            if (it>=100000):
+                break
 
     elif (soma < n_distr):
         while (soma != n_distr):
@@ -43,41 +44,52 @@ def jefferson(votes, result , n_distr, patt_div_r, n_decimal, inc, n_el):
                 patt_div_r = patt_div_r + 3 * inc
                 inc = inc / 100
                 n_decimal_it += 1
-                if (it>=10000000):
-                    break
-        
+
+            if (it>=100000):
+                break
+                
     filepath = "jef_data.csv"           
     if not os.path.exists(filepath):
-        heading = pd.DataFrame(["total","to_distribute","n_elements","std_div_mod"]) # total,to_distribute,n_elements,std_div_mod
-        heading.to_csv('jef_data.csv', index=True)
+        heading = pd.DataFrame(columns=["id","total","to_distribute","n_elements","std_div_mod","len_votes","mean_dev"]) 
+        heading.to_csv('jef_data.csv', index=False)
+        max_idx = 0
+    else:
+        df = pd.read_csv('jef_data.csv')
+        max_idx = df["id"].max()
+    max_idx = max_idx+1
 
-    df = pd.read_csv('jef_data.csv')
-    max_idx = df["id"].max()
-    data_save = [max_idx+1,soma,n_distr,n_el,patt_div_r, len(votes)] 
+    data_save = [max_idx,soma,n_distr,n_el,patt_div_r, len(votes), round(mean_dev,4)]
     file = open(filepath,'a')
     file.write(f'\n{str(data_save).replace("[","").replace("]","")}')
     file.close()
 
-for i in range(100):
-    
-    votes = [rd.randint(0,100), rd.randint(0,100), rd.randint(0,100), \
-                 rd.randint(0,100), rd.randint(0,100)]
-    print(votes)
-    n_el = np.sum(votes) # Number of elements
-    n_distr = 15 # Number of elements to distribute
+for i in [156]:
+    max_ = i
+    min_ = max_ - 6
+    for k in [35]:
+        for j in range(10000):
+        
+            votes = [np.floor(min_+(max_-min_)*round(rd.randint(0,100)/100,1)), 
+                     np.floor(min_+(max_-min_)*round(rd.randint(0,100)/100,1)), 
+                     np.floor(min_+(max_-min_)*round(rd.randint(0,100)/100,1)), 
+                     np.floor(min_+(max_-min_)*round(rd.randint(0,100)/100,1))]
+            
+            n_el = np.sum(votes) # Number of elements
+            mean_dev = np.mean(np.abs(votes - np.mean(votes))) / n_el
+            n_distr = k # Number of elements to distribute
 
-    std_div = compute_pattern_divisor(n_el, n_distr) # Standard divisor
-    n_decimal = 4 # get n. decimal    
+            std_div = compute_pattern_divisor(n_el, n_distr) # Standard divisor
+            n_decimal = 4 # get n. decimal    
 
-    inc = round(std_div, n_decimal) / 1000 # value to in(de)crement on each iteration
+            inc = round(std_div, n_decimal) / 1000 # value to in(de)crement on each iteration
 
-    result = votes / round(std_div, n_decimal) # Standard quote for each member
-    result = [round(elem, n_decimal) for elem in result ] # Standard quote for each member (rounded)
+            result = votes / round(std_div, n_decimal) # Standard quote for each member
+            result = [round(elem, n_decimal) for elem in result ] # Standard quote for each member (rounded)
 
-    method = "Jefferson" # Check the method
+            method = "Jefferson" # Check the method
 
-    if method=="Jefferson":
-        jefferson(votes, result, n_distr, round(std_div, n_decimal), n_decimal, inc, n_el)
+            if method=="Jefferson":
+                jefferson(votes, result, n_distr, round(std_div, n_decimal), n_decimal, inc, n_el, mean_dev)
 
 
 
